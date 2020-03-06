f923b0ea596ba385bdbd85a78b3a0f2691e21a8d
oleg <oleg@eljur.team>
2020-03-06 14:35:25 +0300

    add property

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Post
+ * @package App
+ * @property int id
+ * @property int author_id
+ * @property string title
+ * @property string short_title
+ * @property string img
+ * @property string descr
+ */
 class Post extends Model
 {
     protected $primaryKey = 'post_id';
+
+    protected $table = 'posts';
+
+    protected $fillable = ['id','author_id', 'title', 'short_title', 'img', 'descr'];
+
+    protected $casts = [
+        'id' => 'integer',
+        'author_id' => 'integer',
+    ];
 }
